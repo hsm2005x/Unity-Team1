a56d56c2a2aef859f0c19df903224c4fb083694f
vrooming13 <vrooming13@gmail.com>
2021-01-25 20:58:20 +0900

    2021.01.25 20:56 Commit
    StartScene
    StoryScene
    InGame : Navigation Basic

---
+++ IndiGame_Team1/Assets/1_Story_Scene/Script/InGameButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+public class InGameButton : MonoBehaviour
+{
+    string SceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+    public void LoadStoryScene()
+    {
+        SceneManager.LoadScene("1_StoryScene");
+    }
+    public void LoadIngameScene()
+    {
+        SceneManager.LoadScene("2_IngameScene");
+    }
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
---
+++ IndiGame_Team1/Assets/Scenes/EnemyMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMove : MonoBehaviour
+{
+    NavMeshAgent navAgent;
+    [SerializeField]
+    Transform target;
+    void Awake(){
+        navAgent = GetComponent<NavMeshAgent>();
+        Debug.Log("awake");
+    }
+
+      // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            navAgent.SetDestination(target.position);
+        }
+        Debug.Log("Update");
+    }
+}
