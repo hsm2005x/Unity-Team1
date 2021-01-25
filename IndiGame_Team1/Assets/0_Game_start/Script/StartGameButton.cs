a2b4749c9f24e2d88d1bccfce767c0e49a9f2a51
vrooming13 <vrooming13@gmail.com>
2021-01-25 21:12:48 +0900

    2021.01.25 21:12 파일동기화
    2021.01.25 21:12 파일동기화
    -오광식

---
+++ IndiGame_Team1/Assets/0_Game_start/Script/StartGameButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+public class StartGameButton : MonoBehaviour
+{
+    string SceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+    public void LoadStoryScene()
+    {
+        SceneManager.LoadScene("1_StoryScene");
+    }
+    public void Loadgame()
+    {
+        SceneManager.LoadScene("1_StoryScene");
+    }
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
